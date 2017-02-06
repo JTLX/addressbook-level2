bb408c939255dbf5a42a138a671fbdeddcc62305
Joseph Tang <josephtang765@gmail.com>
2017-02-07 01:00:57 +0800

    Create Printable class.

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,8 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface for printables such as Name, Phone, Email, and Address
+ */
+public interface Printable {
+    public String getPrintableString();
+}
