3c65e5e2394c17211451a87902e9c1535cd196b9
Joseph Tang <josephtang765@gmail.com>
2017-02-12 02:28:04 +0800

    Create test differentlyCasedNames

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 import seedu.addressbook.data.exception.IllegalValueException;
 
 public class NameTest {
+
     @Test
     public void equalNames() throws IllegalValueException {
         Name name1 = new Name("John");
@@ -27,6 +28,25 @@ public class NameTest {
         name2 = new Name("John Do");
         assertFalse(name1.isSimilar(name2));
         assertFalse(name2.isSimilar(name1));
-
     }
+
+    @Test
+    public void differentlyCasedNames() throws IllegalValueException {
+        Name name1 = new Name("John Doe");
+        Name name2 = new Name("JOhN doe");
+        assertTrue(name1.isSimilar(name2));
+        assertTrue(name2.isSimilar(name1));
+
+        name1 = new Name("john doe");
+        name2 = new Name("JOHN DOE");
+        assertTrue(name1.isSimilar(name2));
+        assertTrue(name2.isSimilar(name1));
+
+
+        name1 = new Name("jon doe");
+        name2 = new Name("jon do");
+        assertFalse(name1.isSimilar(name2));
+        assertFalse(name2.isSimilar(name1));
+    }
+
 }
