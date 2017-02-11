2d57cf2947c88664dc818667380d851a99fdd24e
Joseph Tang <josephtang765@gmail.com>
2017-02-12 02:23:25 +0800

    Create test equalNames

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -1,5 +1,32 @@
 package seedu.addressbook.data.person;
 
-public class NameTest {
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class NameTest {
+    @Test
+    public void equalNames() throws IllegalValueException {
+        Name name1 = new Name("John");
+        Name name2 = new Name("John");
+        assertTrue(name1.isSimilar(name2));
+        assertTrue(name2.isSimilar(name1));
+
+        name1 = new Name("John Doe");
+        name2 = new Name("John Doe");
+        assertTrue(name1.isSimilar(name2));
+        assertTrue(name2.isSimilar(name1));
+
+        name1 = new Name("John");
+        name2 = new Name("Jon");
+        assertFalse(name1.isSimilar(name2));
+        assertFalse(name2.isSimilar(name1));
+
+        name1 = new Name("John Doe");
+        name2 = new Name("John Do");
+        assertFalse(name1.isSimilar(name2));
+        assertFalse(name2.isSimilar(name1));
+
+    }
 }
